6b3881c2d12c920d286be913ae8ee106d7ea7a39
Susana <hernandeznerisusanaisabel@gmail.com>
2023-06-13 16:44:44 -0600

    part2a terminada

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,45 +1,19 @@
-import { useState } from 'react'
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return(
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const Note = ({ note }) => {
   return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
+    <li>{note.content}</li>
   )
 }
 
-
-const Display = props => <div>{props.value}</div>
-
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-
-
-const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = newValue => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
-
+const App = ({ notes }) => {
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <h1>Notes</h1>
+      <ul>
+       {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }

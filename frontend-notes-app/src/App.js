66385d8362789440929eba95ab660d66ecb6e888
Susana <hernandeznerisusanaisabel@gmail.com>
2023-06-26 12:35:47 -0600

    part-2b: formulario

--- frontend-notes-app/src/App.js
+++ frontend-notes-app/src/App.js
@@ -1,48 +1,54 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import { useState } from 'react';
+import Note from './components/Note';
 
+//aqui la sintaxis { notes: findNotes } indica que se lecambia el nombre en la desestructuracion
+const App = ({ notes: findNotes }) => {
+  //el estado es una caracteristica que permite almacenar y gestionar la informacion que cambia en una aplicacion, el estado pertenece al componente en el que se encuentra, cada vez que se cambia el estado, el componente se renderiza nuevamente
+  const [notes, setNotes] = useState(findNotes);
+  const [newNote, setNewNote] = useState('una nueva nota...');
+  const [showAll, setShowAll] = useState(true)
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
-
+  //el evento es un objeto
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: notes.length + 1,
-    }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
+    };
 
-  const handleNoteChenge = (event) => {
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = (event) => {
     console.log(event.target.value);
-    setNewNote(event.target.value)//guarda el vaor capturado 
-  }
-
+    setNewNote(event.target.value);
+  };
+  //operador ternario en conjunto de un filtro
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
+  //filter hace un nuevo arreglo 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+      {notesToShow.map(note => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
       <form onSubmit={addNote}>
-        <input 
-        value={newNote} 
-        onChange={handleNoteChenge}
-        />
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>   
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App 
+export default App;

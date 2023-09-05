907719dae6437d7d3904556b60f3b03b2751bfd8
Susana <hernandeznerisusanaisabel@gmail.com>
2023-09-05 11:04:07 -0600

    obteniendo un solo recurso

--- backend-notes-app/index.js
+++ backend-notes-app/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
 let notes = [
   {
@@ -15,30 +16,67 @@ let notes = [
   },
   {
     id: 3,
-   content: "GET and POST are the most important methods of HTTP protocol",
-   important: true
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
-// app.get('/api/notes/:id', (request, response) => {
-//   const id = request.params.id
-//   const note = notes.find(note => {
-//     console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-//     return note.id === id
-//   })
-//   console.log(note)
-//   response.json(note)
-// })
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
 
-const PORT = 3001
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 4000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
